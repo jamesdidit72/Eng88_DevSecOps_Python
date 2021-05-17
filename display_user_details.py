ab09cc0e06f68893b3785bdbff61335df5d28b82
jamesdidit72 <84332658+jamesdidit72@users.noreply.github.com>
2021-05-17 16:07:09 +0100

    displaying_user_details

---
+++ display_user_details.py
@@ -0,0 +1,15 @@
+# asking what the users name is
+name = input("Welcome, what is your name:  ")
+# welcoming the user
+print("Welcome", name)
+# asking the user for their age
+age = int(input("How old are you:  "))
+
+# asking the user for their DOB
+dob = (input("What is your date of birth:  "))
+
+# printing details
+print("Your details are as follows:")
+print("Name:", name)
+print("Age:", age)
+print("Date of birth:", dob)
