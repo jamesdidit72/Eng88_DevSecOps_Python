b772a50ef1e9571e0ac38f6bf2ab88bc7b91fe8f
jamesdidit72 <84332658+jamesdidit72@users.noreply.github.com>
2021-05-17 16:11:44 +0100

    distinguishing_variables

---
+++ variables.py
@@ -0,0 +1,20 @@
+
+first_name = "James"  # string
+last_name = "Barton"  # string
+age = 22  # integer
+wage = 1600.56  # float
+print(first_name)
+print(last_name)
+print(age)
+print(wage)
+print(first_name, last_name, age, wage)
+
+# how to find the variable
+print(type(first_name))
+print(type(last_name))
+print(type(age))
+print(type(wage))
+
+# user input
+name = input("Please enter your name:  ")
+print("Hello", name)
